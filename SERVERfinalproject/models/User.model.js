564e4e8759926e0149459f62f573d0d932917fe6
Pablo Molleda <pmolledadelrio@gmail.com>
2021-07-19 16:43:42 +0200

    done

--- SERVERfinalproject/models/User.model.js
+++ SERVERfinalproject/models/User.model.js
@@ -1,10 +1,21 @@
-{username: String,
-passwod: String,
-role:String,
-teacherInfo: {
-    type: "TeacherSchema"
-},
-studentInfo: {
-    type: "StudentSchema"
-}
-}
+const { Schema, model } = require("mongoose")
+const Teacher = require('./Teacher.model')
+const Student = require('./Student.model')
+
+const userSchema = new Schema({
+
+    userName: { type: String, required: true },
+
+    password: { type: String, required: true },
+
+    isTeacher: { Teacher },
+
+    isStudent: { Student },
+
+    role: { type: String, enum: ['student', 'teacher', 'admin']}
+
+})
+
+const User = model("Admin", userSchema)
+
+module.exports = User
