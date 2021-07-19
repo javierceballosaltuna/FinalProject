f64ef1915aa7fa97c8a931bb4d3f3f1082bfc848
Pablo Molleda <pmolledadelrio@gmail.com>
2021-07-19 16:08:45 +0200

    done

--- SERVERfinalproject/config/index.js
+++ SERVERfinalproject/config/index.js
@@ -1,19 +1,6 @@
 const express = require("express")
 const logger = require("morgan")
 const cookieParser = require("cookie-parser")
-<<<<<<< HEAD
-const favicon = require("serve-favicon")
-
-const path = require("path")
-
-module.exports = (app) => {
-  app.use(logger("dev"))
-
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: false }))
-  app.use(cookieParser())
-}
-=======
 
 module.exports = (app) => {
   app.use(logger("dev"))
@@ -22,4 +9,3 @@ module.exports = (app) => {
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
 }
->>>>>>> 94b26848fd8fa4886e524217505717d8840ff95a
--- SERVERfinalproject/routes/base.routes.js
+++ SERVERfinalproject/routes/base.routes.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
+const express = require('express')
 const router = require("express").Router()
 
-router.get("/", (req, res, next) => {
-    res.render("index")
-})
-=======
-const express = require('express')
-const router = require("express").Router();
 
-
-/* GET home page */
 router.get("/", (req, res, next) => {
-    res.send('tomamos el controllllllllllll');
+    res.send('tomamos el controllllllllllll')
 });
->>>>>>> 94b26848fd8fa4886e524217505717d8840ff95a
 
 module.exports = router
