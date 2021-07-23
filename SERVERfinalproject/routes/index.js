b7782d1897f9ea2b1744633a4f14291bcc498165
Pablo Molleda <pmolledadelrio@gmail.com>
2021-07-23 10:05:30 +0200

    done

--- SERVERfinalproject/routes/index.js
+++ SERVERfinalproject/routes/index.js
@@ -1,7 +1,6 @@
 module.exports = (app) => {
   
   app.use('/', require('./auth.routes'))
-  app.use('/', require('./admin.routes'))
   app.use('/', require('./subjects.routes'))
   app.use('/profile', require('./profile.routes'))
   app.use('/events', require('./event.routes'))
